e58d7440e688396d897557c45d661efee3192067
young1ll <122770896+young1ll@users.noreply.github.com>
2023-03-30 18:50:47 +0900

    Add files via upload

---
+++ baekjoon/BAEKJOON_10871.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BAEKJOON_10871 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String stArr1[] = br.readLine().split(" ");
+		String stArr2[] = br.readLine().split(" ");
+		
+		int numArr1[] = new int[2];
+		int numArr2[] = new int[stArr2.length];
+		for(int i=0; i<stArr1.length; i++) numArr1[i] = Integer.parseInt(stArr1[i]);
+		for(int i=0; i<stArr2.length; i++) numArr2[i] = Integer.parseInt(stArr2[i]);
+
+		List<Integer>result = new ArrayList<>();
+		for(int i=0; i < stArr2.length; i++) {
+			if(numArr2[i] < numArr1[1]) result.add(numArr2[i]);
+		}
+		for(int r : result) System.out.print(r + " ");
+	}
+}
---
+++ baekjoon/BAEKJOON_10989.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.io.*;
+
+public class BAEKJOON_10989 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] a = new int[n];
+		int[] counting = new int[10001];
+		int[] result = new int[n];
+		for(int i=0; i<n; i++) a[i] = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<n; i++) counting[a[i]]++;
+		for(int i=1; i<counting.length; i++) counting[i] += counting[i-1];
+		for(int i = n-1; i>=0; i--) {//뒤에서부터
+			int v = a[i];
+			counting[v]--;
+			result[counting[v]] = v;
+		}
+		
+		for(int i=0; i<result.length; i++) {
+			bw.write(String.valueOf(result[i]) +"\n");
+		}
+		bw.flush();
+		
+	}
+}
