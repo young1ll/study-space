aa2b54645472a9236af3650191ab64e535ad5ad5
young1ll <122770896+young1ll@users.noreply.github.com>
2023-03-30 12:33:03 +0000

    git test

---
+++ baekjoon/BAEKJOON_11399.java
@@ -0,0 +1,14 @@
+package baekjoon;
+/** ATM - 그리디 알고리즘
+// P_i분
+// 입력 : 첫 줄에 사람의 수 N(1<=N<=1000), 각 사람의 인출 시간(P_i)
+// 5
+// 3 1 4 3 2
+// 출력 : 각 사람이 돈을 인출하는데 필요한 시간의 합의 최솟값
+// 32
+**/
+public class BAEKJOON_11399 {
+    public void main(String[] args) {
+        System.out.println("hello");
+    }
+}
