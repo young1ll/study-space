783e40d0754c99af03a6b7ff3844c443dd5f8112
young1ll <122770896+young1ll@users.noreply.github.com>
2023-04-01 11:01:52 +0000

    알파벳 찾기

---
+++ baekjoon/BAEKJOON_10809.java
@@ -0,0 +1,36 @@
+// package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class BAEKJOON_10809 {
+    public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();//어떤 단어 입력
+		StringBuilder strB = new StringBuilder();
+		
+		for(int i=0; i<str.length(); i++) {
+			char c = str.charAt(i);
+			if (strB.indexOf(String.valueOf(c)) == -1) {
+        		strB.append(c);
+			}
+			else strB.append('0');
+		}
+		
+		for(int i=97; i<123; i++) {
+			boolean checker = false;
+			for(int j=0; j<strB.length(); j++) {
+				if(strB.charAt(j) == i) {
+					System.out.print(j + " ");
+					checker = true;
+					break;
+				}
+			}
+			if(!checker) {
+				System.out.print("-1 ");
+			}
+		}
+	}//main
+}
