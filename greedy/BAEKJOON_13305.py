612fc0e84e010bb0ed7124aecca76f6addce2e55
young1ll <goooglecho01@gmail.com>
2023-04-05 04:39:48 +0900

    New file solved in Week01

--- greedy/BAEKJOON_13305.py
+++ greedy/BAEKJOON_13305.py
@@ -3,4 +3,8 @@ n = int(input())
 road = list(map(int, input().split()))
 price = list(map(int, input().split()))
 
-p = price[0]
+result, min = 0, price[0]
+for r, p in zip(road, price):
+   if p < min: min = p
+   result += min * r
+print(result)
