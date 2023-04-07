eaf683342c68e73d6c15d2205116067ed44b45f9
young1ll <goooglecho01@gmail.com>
2023-04-08 03:54:10 +0900

    Add : new file > 각 알파벳을 미지수로 생각해보면, 주어진 예시를 다음과 같이 풀이할 수 있다. GCF + ACDEB = G*(100) + C*(10) + F*(1) A*(10000) + C*(1000) + D*(100) + E*(10) + B*(1)
    ### 구현
    1. n 입력 받기
    2. 문자입력 받기, 자릿수와 함께 Map에 저장
    3. 저장 시, 키(문자) 체크...중복 시 값 더하기
    4. 값 기준으로 정렬
    5. Entry 순회하며 result 도출

--- greedy/BAEKJOON_1339.java
+++ greedy/BAEKJOON_1339.java
@@ -1,7 +1,5 @@
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -22,63 +20,48 @@ public class BAEKJOON_1339 {
     // 2. 저장된 배열 내 문자열의 알파벳 마다 자릿 수 구하기
     // 3. 높은 문자열에 높은 수를 대입하고 자릿수와 곱하기
     // 4. 모두 더하기
-
+    // ============================================================
     // 1. n 입력 받기
-    // 2. 문자열 배열로 입력 받기
-    // 3. 문자열 자릿수가 높은 문자열을 자릿수와 문자를 가진 맵으로 저장
-    // 4. 같은 키 값을 가졋다면 자릿수 더하기
+    // 2. 문자열 char로 입력 받기, 자릿수도 필요하기 때문에 Map으로..
+    // 3. 저장할 때 키 체크 : 중복 시 값을 더하기
+    // 4. 값 기준으로 Map 정렬 내림차
+    // 5. 같은 키 값을 가졋다면 자릿수 더하기
+    // + Entry 이해하기!
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        List<Map<Character, Integer>> list = new ArrayList<>();
         Map<Character, Integer> map = new HashMap<>();
-        
         int result = 0;
-        
-        for(int i=0; i < n; i++) {
-            String str = sc.next();
-            int digit = 0;
-            for(int j=str.length()-1; j >= 0; j--) {
-                map.put( str.charAt(j), (int) Math.pow(10, digit) );
-                for(Map<Character, Integer> c : list) {
-                    
-                }
-                
-                digit++;
 
-                list.add(map);
+        for(int i=0; i < n; i++) {
+            String s = sc.next();
+            int digit = 0;
+            for(int j=s.length()-1; j >=0; j--) {
+                int num = (int) Math.pow(10, digit);
+
+                if(map.containsKey(s.charAt(j))) {
+                    map.put(s.charAt(j), map.get(s.charAt(j)) + num);
+                }
+                else map.put(s.charAt(j), num);
+                digit++;
             }
         }
-        
-        // // sort
-        // Collections.sort(list, new Comparator<Map<Character, Integer>>() {
-        //     @Override
-        //     public int compare(Map<Character, Integer> o1, Map<Character, Integer> o2) {
-        //         Integer v1 = o1.values().iterator().next();
-        //         Integer v2 = o2.values().iterator().next();
 
-        //         if(v2.compareTo(v1) == 0) {
-        //             Character k1 = o1.keySet().iterator().next();
-        //             Character k2 = o2.keySet().iterator().next();
-        //             return k1.compareTo(k2);
-        //         }
-        //         return v2.compareTo(v1);
-        //     }
-        // });
+        // sort : Map to List
+        List<Map.Entry<Character, Integer>> enList = new ArrayList<>(map.entrySet());
+        enList.sort(new Comparator<Map.Entry<Character, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Character, Integer> o1, Map.Entry<Character, Integer> o2) {
+                return o2.getValue() - o1.getValue();
+            }
+        });
 
+        int num = 9;
+        for(Map.Entry<Character, Integer> m : enList) {
+            result += m.getValue() * num;
+            num--;
+        }
 
-        // for(Map<Character, Integer> m : list) {
-        //     int num = 9;
-        //     for(Map.Entry<Character, Integer> en : m.entrySet()) {
-        //        if( en.getKey() == list.get(n) )
-        //     }
-        // }
-
-        // System.out.println(Arrays.toString(str));
-        System.out.println(list);
         System.out.println(result);
-
-
     }
-
 }
