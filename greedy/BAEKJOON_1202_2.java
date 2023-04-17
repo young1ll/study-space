5b88016adeca9129c0c5f3f11539e40f88687260
young1ll <goooglecho01@gmail.com>
2023-04-17 10:58:45 +0900

    Solved: BAEKJOON_1202(보석도둑) - PriorityQueue를 사용한 이전 풀이와 유사한 과정이지만 중요한 차이가 있었다. - Comparator.comparing 활용 참고 - AbstractMap.SimpleEntry 활용 참고 - for(int bag : bags) 구문 참고

--- greedy/BAEKJOON_1202_2.java
+++ greedy/BAEKJOON_1202_2.java
@@ -1,22 +1,22 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class BAEKJOON_1202_2 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        Map<Integer, Integer> map = new TreeMap<>(Collections.reverseOrder());
-
+        List<Map.Entry<Integer, Integer>> items = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            int m = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            map.put(m, v);
+            int weight = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+            items.add(new AbstractMap.SimpleEntry<>(weight, value));
         }
+        items.sort(Comparator.comparingInt(Map.Entry::getKey));
 
         int[] bags = new int[k];
         for (int i = 0; i < k; i++) {
@@ -24,18 +24,30 @@ public class BAEKJOON_1202_2 {
         }
         Arrays.sort(bags);
 
+        PriorityQueue<Map.Entry<Integer, Integer>> pq =
+                new PriorityQueue<>(Comparator.comparing(Map.Entry<Integer, Integer>::getValue).reversed());
+                //(::) ...s -> ...s.getValue().reversed() 
         long result = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-
-        for (int i = 0, j = 0; i < k; i++) {
-            while (j < n && map.containsKey(map.keySet().toArray()[j]) && (int) map.keySet().toArray()[j] <= bags[i]) {
-                pq.offer(map.get(map.keySet().toArray()[j++]));
+        int itemIndex = 0;
+        for (int bag : bags) {
+            while (itemIndex < n && items.get(itemIndex).getKey() <= bag) {
+                pq.offer(items.get(itemIndex++));
             }
             if (!pq.isEmpty()) {
-                result += pq.poll();
+                result += pq.poll().getValue();
             }
         }
-
         System.out.println(result);
     }
 }
+// 반례
+// 3 3
+// 20 12
+// 0 20
+// 16 16
+// 17
+// 14
+// 7
+
+// [x] 20
+// [o] 36
